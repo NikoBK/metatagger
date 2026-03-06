88d95b38ad2db20a222d9beab76a4832bf75e9fd
NikoBK <112914618+NikoBK@users.noreply.github.com>
2026-03-06 23:09:48 +0100

    test shape iteration

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -2,5 +2,9 @@
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public string? FilePath { get; set; }
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public string? Description { get; set; }
+    public string? CoverPath { get; set; }
 }
