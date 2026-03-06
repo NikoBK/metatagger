0649b6082305f109f6d56e4885318f6fb7a38e12
NikoBK <112914618+NikoBK@users.noreply.github.com>
2026-03-06 23:40:41 +0100

    header buttons, file browser and control enabling

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -7,4 +7,13 @@ public partial class MainWindowViewModel : ViewModelBase
     public string? Author { get; set; }
     public string? Description { get; set; }
     public string? CoverPath { get; set; }
+
+    private bool _isFileLoaded;
+    public bool IsFileLoaded
+    {
+        get => _isFileLoaded;
+        set => SetProperty(ref _isFileLoaded, value);
+    }
+
+    public bool IsFolderLoaded {get; set; } = false;
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,10 @@
+using System;
+
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using metatagger.ViewModels;
+using TagLib;
 
 namespace metatagger.Views;
 
@@ -7,5 +13,46 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private async void LoadAudioFile_Click(object? sender, RoutedEventArgs e)
+    {
+        var files = await StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Select audio file",
+            AllowMultiple = false,
+            FileTypeFilter = new[]
+            {
+                new FilePickerFileType("Audio files")
+                {
+                    Patterns = new[] { "*.mp3", "*.flac", "*.ogg", "*.m4a", "*.wav" }
+                }
+            }
+        });
+
+        if (files.Count == 0) {
+            Console.WriteLine("Error reading!");
+            return;
+        }
+        else {
+            Console.WriteLine("Success!");
+        }
+
+        var path = files[0].TryGetLocalPath();
+        if (string.IsNullOrEmpty(path))
+            return;
+
+        var vm = (MainWindowViewModel)DataContext!;
+
+        vm.FilePath = path;
+
+        var audioFile = TagLib.File.Create(path);
+
+        vm.Title = audioFile.Tag.Title ?? "";
+        vm.Author = audioFile.Tag.FirstPerformer ?? "";
+        vm.Description = audioFile.Tag.Comment ?? "";
+
+        vm.IsFileLoaded = true;
+        Console.WriteLine($"fileloaded set to: {vm.IsFileLoaded}");
     }
 }
